998558f1ebdb1139cc7c3cf5ac3ff21c4722ac09
Chelak Ilya <45993348+Diahr@users.noreply.github.com>
2019-01-24 12:13:09 +0300

    Add files via upload

---
+++ Weiszfeld.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "EvaluateMedian.h"
+
+namespace median {
+	/*
+	 Weiszfeld algorithm for finding the median.
+	*/
+	class Weiszfeld : public EvaluateMedian {
+	public:
+		arma::rowvec getMedian(arma::mat setOfPoints, size_t iterations);
+	};
+}
